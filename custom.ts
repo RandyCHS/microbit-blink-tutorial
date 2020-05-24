a32647bb4d532c66afa7d7ab975722796b762661
Randy Macdonald <41399033+RandyCHS@users.noreply.github.com>
2020-05-23 22:36:04 -0700

    bounds on x & y

--- custom.ts
+++ custom.ts
@@ -10,6 +10,9 @@ namespace animations {
      * @param interval time in milliseconds between blinks
      */
     //% block="blink x $x y $y every $interval ms"
+    //% x.min=0 x.max=4 y.min=0 y.max=4
+    //% x.fieldOptions.precision=1 y.fieldOptions.precision=1
+
     export function  blink(x: number, y: number, interval: number): void {
         let sprite = game.createSprite(x, y)
         sprite.setBlink(interval)
